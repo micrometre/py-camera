0bc5d417eb6de060dba668349257ae8586a9b423
Henok <henokmicrometre@gmail.com>
2024-02-24 11:50:01 +0000

    added vlc

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 .PHONY: run
 flask_start:
 	flask run --host=0.0.0.0 --debug
+
+stream_vlac:
+	 vlc alprVideo.mp4 --sout "#duplicate{dst=std{access=http,mux=ts,dst=127.0.0.1:8080}}"
--- movie.py
+++ movie.py
@@ -3,9 +3,10 @@ import subprocess
 def convert_avi_to_mp4():
     input_file = 'tmp/output.avi'
     input_arg1 = "-i"
-    input_arg2 = "-c:v"
+    input_arg2 = "-c:a"
     input_arg3 = "mpeg4"
-    input_arg4 = "video.mp4"
+    input_arg4 = "tmp/output.mp4"
     output = subprocess.check_output(['ffmpeg', str(input_arg1), str(input_file), str(input_arg2), str(input_arg3), str(input_arg4) ])
+#ffmpeg -i output.avi -c:v copy -c:a copy -y output.mp4
 
 convert_avi_to_mp4()
